e7d9a49b0a915be1b939f13060ccbceea4f7c94f
Leninux <redrudeboy@gmail.com>
2015-03-09 21:29:10 +0100

    Adding old versions, challenger-modal & more images.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 $( document ).ready(function() {
+
+/****************\
+ * NAVBAR       *
+\****************/
+    $(".goToCalendars").click(function() {
+        $("#page-challenger,#page-challenger-modal,#page-charts").addClass("hide");
+        $("#page-todo,#page-events,#page-filters").removeClass("hide");
+    });
+
+    $(".goToChallenger").click(function() {
+        $("#page-challenger-modal,#page-todo,#page-events,#page-filters,#page-charts").addClass("hide");
+        $("#page-challenger").removeClass("hide");
+    });
+
+    $(".goToChallengerModal").click(function() {
+        $("#page-challenger,#page-todo,#page-events,#page-filters,#page-charts").addClass("hide");
+        $("#page-challenger-modal").removeClass("hide");
+    });
+
+
+
+
 /****************\
  * CHALLENGER   *
 \****************/
@@ -10,10 +32,10 @@ $( document ).ready(function() {
 
     // initialize Isotope
     var $containerChallenger = $('#page-challenger-masonry-container').isotope({
-      itemSelector: '.vcomponent-item',
+      itemSelector: '#page-challenger-masonry-container .vcomponent-item',
       // initialize Masonry
       masonry: {
-          columnWidth: 100
+          columnWidth: 200
       }
     });
 
@@ -43,18 +65,23 @@ $( document ).ready(function() {
             // filter function
             window.tag = $( this ).attr('data-filter-tag');
             filterValue = function(tag) {
-                var tags = $(this).find('.p-category').data('tags').split(";"); //or .attr('data-tags');
-                for (var i = 0, len = tags.length; i < len; i++) {
-                    if(window.tag == tags[i]) {
-                        return true;
+                if( $("#challenger-toolbar button.active").length === 0 ) {
+                    return true;
+                } else if( $(this).find('.p-category').length > 0 ) {
+                    var tags = $(this).find('.p-category').data('tags').split(";"); //or .attr('data-tags');
+                    for (var i = 0, len = tags.length; i < len; i++) {
+                        if(window.tag == tags[i]) {
+                            return true;
+                        }
                     }
+                } else {
+                    return false;
                 }
-                return false;
             };
         } else {
             window.tag = false;
         }
-        $container.isotope({ filter: filterValue });
+        $containerChallenger.isotope({ filter: filterValue });
     });
 
     // change is-checked class on buttons
@@ -83,12 +110,7 @@ $( document ).ready(function() {
         $(this).toggleClass("active");
         $("#challenger-toolbar-body").toggleClass("hide");
     });
-     $(".diary-button").click(function() {
-        if($(".list-unstyled").hasClass("hide")) {
-            $(".list-unstyled").removeClass("hide");
-            $("form").removeClass("hide");
-        }
-    });
+
 /*    $("#challenger-toolbar #challenger-toolbar-btn-pleasure").click(function() {
         $("#challenger-toolbar #challenger-toolbar-pleasure").toggleClass("hide");
     });
@@ -98,10 +120,66 @@ $( document ).ready(function() {
 
 
 
+
+/**********************\
+ * CHALLENGER MODAL   *
+page-challenger-modal
+    page-challenger-modal-periodicity
+        challenger-modal-periodicity-btn-annual
+        challenger-modal-periodicity-btn-monthly
+        challenger-modal-periodicity-btn-weekly
+        //challenger-modal-periodicity-btn-daily
+
+    page-challenger-modal-periodicity-annual
+        challenger-modal-periodicity-annual-btn-one
+        challenger-modal-periodicity-annual-btn-nine
+        challenger-modal-periodicity-annual-btn-six
+        challenger-modal-periodicity-annual-btn-three
+
+    page-challenger-modal-periodicity-monthly
+        challenger-modal-periodicity-monthly-btn-one
+        challenger-modal-periodicity-monthly-btn-three
+        challenger-modal-periodicity-monthly-btn-two
+        challenger-modal-periodicity-monthly-btn-four
+
+    page-challenger-modal-periodicity-weekly
+        challenger-modal-periodicity-weekly-btn-monday
+        challenger-modal-periodicity-weekly-btn-tuesday
+        challenger-modal-periodicity-weekly-btn-wednesday
+        challenger-modal-periodicity-weekly-btn-thursday
+        challenger-modal-periodicity-weekly-btn-friday
+        challenger-modal-periodicity-weekly-btn-saturday
+        challenger-modal-periodicity-weekly-btn-sunday
+\**********************/
+
+    $("#challenger-modal-periodicity-btn-annual").click(function() {
+        $("#page-challenger-modal-periodicity button").removeClass("active");
+        $(this).addClass("active");
+        $("#page-challenger-modal-periodicity-monthly,#page-challenger-modal-periodicity-weekly").addClass("hide");
+        $("#page-challenger-modal-periodicity-annual").removeClass("hide");
+    });
+
+    $("#challenger-modal-periodicity-btn-monthly").click(function() {
+        $("#page-challenger-modal-periodicity button").removeClass("active");
+        $(this).addClass("active");
+        $("#page-challenger-modal-periodicity-annual,#page-challenger-modal-periodicity-weekly").addClass("hide");
+        $("#page-challenger-modal-periodicity-monthly").removeClass("hide");
+    });
+
+    $("#challenger-modal-periodicity-btn-weekly").click(function() {
+        $("#page-challenger-modal-periodicity button").removeClass("active");
+        $(this).addClass("active");
+        $("#page-challenger-modal-periodicity-annual,#page-challenger-modal-periodicity-monthly").addClass("hide");
+        $("#page-challenger-modal-periodicity-weekly").removeClass("hide");
+    });
+
+
+
 /****************\
  * CALENDAR     *
 \****************/
     var $container = $('#page-todo-packery-container').packery({
+        itemSelector: "#page-todo-packery-container .vcomponent-item",
         columnWidth: 80,
         rowHeight: 80
     });
@@ -114,5 +192,10 @@ $( document ).ready(function() {
         } );
         // bind Draggabilly events to Packery
         $container.packery( 'bindDraggabillyEvents', draggie );
+    });
+
+    $("#page-events .diary-button").click(function() {
+        $("#page-events-diary").toggleClass("hide");
+        $(this).toggleClass("active");
     });
 });
